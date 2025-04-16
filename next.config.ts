eb1d1fd3738c9460845d9cd63fc10014fc56e444
achesolo@gmail.com <achesolo@gmail.com>
2025-04-16 09:26:37 -0400

    v1

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
 };
 
 export default nextConfig;
